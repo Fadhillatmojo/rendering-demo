91fc9c677d3d0fe6e7b7ac78feffcfe4a3a854ae
User <fadhillatmojo@gmail.com>
2024-07-24 14:22:13 +0700

    dynamic rendering

---
+++ src/app/product-detail/page.tsx
@@ -0,0 +1,13 @@
+import { Product } from "@/components/product";
+import { Reviews } from "@/components/reviews";
+
+export default function ProducDetailPage() {
+	return (
+		<div>
+			<h1>Product Detail</h1>
+			<Product />
+			<Reviews />
+
+		</div>
+	);
+}

1e9ea6b50724b76f6fdc534b320e3b35388b0d73
User <fadhillatmojo@gmail.com>
2024-07-25 15:44:07 +0700

    client only code with npm i client-only

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,11 +1,14 @@
 "use client";
+import "client-only"; // agar fungsi server tidak dapat diimport ke dalam sini
 // every client component also rendered once on the server to generate the initial html
 // import { serverSideFunction } from "@/utils/server-utils";
 import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
 import { useTheme } from '@/components/theme-provider';
+import { clientSideFunction } from '@/utils/client-utils';
+// import { serverSideFunction } from "@/utils/server-utils";
 
 export default function ClientRoutePage() {
 	console.log("Client route rendered");
@@ -18,10 +21,12 @@ export default function ClientRoutePage() {
 	// 	</>
 	// );
 	const theme = useTheme();
+	// const result = serverSideFunction();
+	const clientResult = clientSideFunction();
 
-	const settings = {
-		dots: true,
-	};
+	// const settings = {
+	// 	dots: true,
+	// };
 	return (
 		// <div className="image-slider-container">
 		// 	<Slider {...settings}>
@@ -39,10 +44,8 @@ export default function ClientRoutePage() {
 		// 		</div>
 		// 	</Slider>
 		// </div>
-		<h1 style={{
-			color: theme.colors.primary
-		}}>
-			Client Route
+		<h1>
+			Client Route {clientResult}
 		</h1>
 	);
 }
---
+++ src/utils/client-utils.ts
@@ -0,0 +1,7 @@
+export const clientSideFunction = () => {
+	console.log(
+		`use window object,
+		use local storage`
+	);
+	return "Client result";
+};
